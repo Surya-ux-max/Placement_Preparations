e161052cf679ecc00352c1ae9043e16ee57aac03
Suryaprakash S <surprakas14@gmail.com>
2025-12-22 21:02:59 +0530

    Create Largest_element_in_the_array

---
+++ Largest_element_in_the_array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    
+    int n=5;
+    int largest;
+    
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    int m = arr[0];
+    
+    for(int i=0;i<n;i++){
+        if(arr[i]>m){
+            m = arr[i];
+        }
+    }
+    cout<<m<<endl;
+    
+    return 0;
+}
