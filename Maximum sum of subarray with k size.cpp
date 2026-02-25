48420675442fe96e5b6edeae0e0ab0d7569b5a54
Suryaprakash S <surprakas14@gmail.com>
2026-02-25 09:26:21 +0530

    Update Maximum sum of subarray with k size.cpp

--- Maximum sum of subarray with k size.cpp
+++ Maximum sum of subarray with k size.cpp
@@ -27,3 +27,29 @@ int main(){
     return 0;
     
 }
+--------------------------------------------------------------------------
+Another solution -
+    #include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int k;
+    cin>>n>>k;
+    
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int currentSum=0;
+    int maxSum=0;
+    
+    for(int i=0;i<=n-k;i++){   //if input k=3,n=6, then  n-k = 6-3 = 3 , first three position
+        int currentSum=0;
+        for(int j=i;j<i+k;j++){    // j=i means 0, i+k means 0+6 = 6 .
+            currentSum+=arr[j];    // 1 = currentSum , then 2 will come inside , then 3 will come inside
+            }
+            maxSum = max(currentSum,maxSum); // here the comparison happens using the max function.
+        }
+    cout<<"maximum sum "<<maxSum;
+}
